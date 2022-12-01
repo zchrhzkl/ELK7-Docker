48e419792b1c8db008aad918294a9d07dba1d59a
Zach <zachariah@pensieve.id>
2022-12-01 10:09:28 +0700

    Fresh

---
+++ setup-vol.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## CREATE DOCKER VOLUME FOLDERS
+#mkdir -p vol/db0{1..3}
+mkdir -p vol/certs
+mkdir -p vol/es{01}/{data,logs}
+mkdir -p vol/kibana/data/
+
+## SETUP PERMISSION AND OWNERSHIP
+sudo chmod -R 755 vol/
+sudo chown -R 1000:1000 vol/
+
+## SETUP VM MAX MAP COUNT
+sudo sysctl -w vm.max_map_count=262144 
+sudo cat <<EOF >> /etc/sysctl.conf
+vm.max_map_count=262144
+EOF
