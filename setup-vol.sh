92018d98b38f5969bf240bc79e50ccbb458d938c
Zach <zachariah@pensieve.id>
2022-12-01 11:17:07 +0700

    Update vol folder

--- setup-vol.sh
+++ setup-vol.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ## CREATE DOCKER VOLUME FOLDERS
-#mkdir -p vol/db0{1..3}
-mkdir -p vol/certs
-mkdir -p vol/es01/{data,logs}
-mkdir -p vol/kibana/data/
+#mkdir -p ~/ELK7/vol/db0{1..3}
+mkdir -p ~/ELK7/vol/certs
+mkdir -p ~/ELK7/vol/es01/{data,logs}
+mkdir -p ~/ELK7/vol/kibana/data/
 
 ## SETUP PERMISSION AND OWNERSHIP
-sudo chmod -R 755 vol/
-sudo chown -R 1000:1000 vol/
+sudo chmod -R 755 ~/ELK7/vol/
+sudo chown -R 1000:1000 ~/ELK7/vol/
 
 ## SETUP VM MAX MAP COUNT
 sudo sysctl -w vm.max_map_count=262144 
